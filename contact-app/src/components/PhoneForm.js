39cf92cad4e027464913f1d82e92b6680fe23d66
minjuice1 <minjuice93@gmail.com>
2022-02-01 21:25:11 +0900

    add 등록하기 버튼을 생성하여 값을 submit

--- contact-app/src/components/PhoneForm.js
+++ contact-app/src/components/PhoneForm.js
@@ -7,21 +7,34 @@ class PhoneForm extends Component {
 	};
 	d;
 
-	// onchange로 값을 전달해줄때, input이 여러개라서 value로 받아지는 데이터가 겹친다면
+	// onchange로 state 값을 전달해줄때, input이 여러개라서 value로 받아지는 데이터가 겹친다면
 	// this.setState 안에 원래는 객체 형식으로
 	// name: e.target.value,
 	// 이런식으로 넣었었는데, 그거 대신 event.target 안의 name 값을 가져온다.
 	// **주의 : input마다 name값을 먼저 주고, 핸들러에 e.target.name을 추가해야 함.
 	ChangeHandler = (e) => {
-		console.log(e);
 		this.setState({
 			[e.target.name]: e.target.value,
 		});
 	};
 
+	SubmitHandler = (e) => {
+		// button을 누르면 렌더링 되는것을 막기 위해.
+		e.preventDefault();
+
+		// 부모 컴포넌트인 app으로부터 onCreate를 받아와서
+		// this.state (name, phone)을 넣어준다.
+
+		// this.props.onCreate({
+		// 	name: this.state.name,
+		// 	phone: this.state.phone,
+		// });
+		this.props.onCreate(this.state);
+	};
+
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.SubmitHandler}>
 				<input
 					name="name"
 					placeholder="name"
@@ -34,6 +47,7 @@ class PhoneForm extends Component {
 					onChange={this.ChangeHandler}
 					value={this.state.phone}
 				/>
+				<button type="submit">등록하기</button>
 				<div>
 					{this.state.name} {this.state.phone}
 				</div>
