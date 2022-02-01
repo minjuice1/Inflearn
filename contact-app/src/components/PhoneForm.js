a90920cc8a97dc88310005366fdd797c56ec5bfd
minjuice1 <minjuice93@gmail.com>
2022-02-01 20:55:11 +0900

    state를 통해서 여러개의 input 값을 변경

--- contact-app/src/components/PhoneForm.js
+++ contact-app/src/components/PhoneForm.js
@@ -7,6 +7,11 @@ class PhoneForm extends Component {
 	};
 	d;
 
+	// onchange로 값을 전달해줄때, input이 여러개라서 value로 받아지는 데이터가 겹친다면
+	// this.setState 안에 원래는 객체 형식으로
+	// name: e.target.value,
+	// 이런식으로 넣었었는데, 그거 대신 event.target 안의 name 값을 가져온다.
+	// **주의 : input마다 name값을 먼저 주고, 핸들러에 e.target.name을 추가해야 함.
 	ChangeHandler = (e) => {
 		console.log(e);
 		this.setState({
